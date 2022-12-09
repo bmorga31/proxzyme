c843ee89efc26ec8cb2f2fbdc14fbb8869d1bf8c
bmorga31 <bmthalia@gmail.com>
2022-12-09 14:41:41 -0500

    corrected a plethora of spelling errors

--- R/data-tween_package_key.R
+++ R/data-tween_package_key.R
@@ -1,6 +1,6 @@
 #' Plate Key for Tween Experiment
 #'
-#' Key for plate layour for twwen experiment spectifying the treatment/condition
+#' Key for plate layout for tween experiment specifying the treatment/condition
 #' of each well
 #'
 #' @format a data frame with 72 rows and 4 columns
@@ -13,7 +13,7 @@
 #' P2 Only, contains only proxzyme 2, with 41 bp ssDNA target
 #' ssDNA T, contains both proxzymes, with 41 bp ssDNA target}
 #' \item{treatment}{the buffer treatment/reaction condition for the corresponding well,
-#' water, containd no additional buffer components (TE buffer, *Salt, 5mg/50ml Hemin, DMSO),
+#' water, contained no additional buffer components (TE buffer, *Salt, 5mg/50ml Hemin, DMSO),
 #' buffer, contains PCR buffer in addition to standard buffer components
 #' 0.05%, 0.005%, 0.0005%, 0.00005%, concentration of Tween added to buffer}
 #' }
