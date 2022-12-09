1887678c978f1c2ad6ed81c8a696908ea97c5a99
bmorga31 <bmthalia@gmail.com>
2022-12-09 14:49:33 -0500

    added another data det and key that was used to test functions, and their documentation

---
+++ R/data-mg_and_edta_data.R
@@ -0,0 +1,14 @@
+#' Proxzyme Reaction with different concentrations of Mg with or wo EDTA
+#'
+#' Absorbance reads from 1 to 32 minutes of Proxzyme reaxtion with TMB substrate.
+#' There were four DNA conditions and eight buffer treatments/reaction conditions.
+#'
+#' @format a data frame containing 88 rows and 14 columns
+#' \describe{
+#' \item{time}{Time at which the absorbance was measured, in minutes}
+#' \item{row}{Lettered rows on the 96 well plate, A-F}
+#' \item{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}
+#' {Numbered columns of the 96 well plate, 1-12}
+#' }
+#'
+"mg_and_edta_data"
